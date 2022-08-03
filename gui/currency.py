836848b8595cafc4f93cb2e65b71e8812799b171
Xizheng (Hugh) Yu <yuxizheng@outlook.com>
2022-08-03 15:20:32 -0500

    integration

---
+++ gui/currency.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import mysql.connector 
+
+def currency_page(root, db, mycur):
+    global search
+    search = Toplevel(root)
+    search.geometry('300x500')
+    search.title('Select a currency') 
+    pass
--- gui/result.py
+++ gui/result.py
@@ -6,16 +6,16 @@ from tkinter import ttk
 import mysql.connector
 
 def graph(db, query):
-    daily = pd.read_sql(sql, db, index_col="date", parse_dates=True)
+    daily = pd.read_sql(query, db, index_col="date", parse_dates=True)
     daily.index = pd.DatetimeIndex(daily.index.values)
     # plot daily candlestick chart with volume
     fig = mpf.plot(daily,type='line',mav=(20),volume=True, style='yahoo')
 
 # test only
-
+'''
 db = mysql.connector.connect(host = "localhost", user = "root", password = "A1b2C3d4&", db ="564project")
 sql = "SELECT a.date, a.open, a.high, a.low, a.close, a.volume FROM assets a, commo b WHERE a.symbol = b.symbol AND b.name = \"Gold\""
 graph(db, sql)
-
+'''
 
 
