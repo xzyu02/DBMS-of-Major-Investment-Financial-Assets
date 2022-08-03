478f482394b4c1ea6a43958caa4e93ea28370cba
Xizheng (Hugh) Yu <yuxizheng@outlook.com>
2022-08-02 20:41:43 -0500

    user registration and login page finished

--- gui/pages.py
+++ gui/pages.py
@@ -1,12 +1,14 @@
 from tkinter import *
 
-def pages(username):
+def pages(user):
     root = Tk()
     root.geometry('300x300')
-    root.title('Welcome %s'.format(username))
+    root.title('Welcome {}'.format(user.get()))
 
     def none():
         pass
+
+    Label(root, text="User Portal", font="bold", bg="grey",fg="black",width=300).pack(ipadx=10, ipady=10, expand=False)
 
     watchlist_button = Button(root, text="Watchlists", command=none, height=1, width = 8)
     watchlist_button.pack(ipadx=10, ipady=10, expand=True)
